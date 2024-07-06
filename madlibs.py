b7822457a977908e1186094135b99ea77806d679
Precious Adigwe <padigwe@charlotte.edu>
2024-07-06 18:01:55 -0400

    Created madlib game based on cooking

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+#    meal = "pizza" 
+#
+#    print("make some " + meal)
+#    print("make some {}".format(meal)) # puts whatever is in format() into the {}
+#    print(f"make some {meal}") # f string
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("verb: ")
+fperson = input("Famous Person: ")
+
+madLib = f"Cooking is so {adj}. I really love to {verb1} while doing it!. Honestly, I need to {verb2} like I'm {fperson}"
+
+print(madLib)
